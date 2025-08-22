2b98a1f42ade182bd8e1d85e05000e264f55eae3
Ajoe62 <ijeborjoe@gmail.com>
2025-08-22 08:30:11 +0100

    chore: add runtime config to public/config.js

---
+++ extension/public/config.js
@@ -0,0 +1,6 @@
+// Runtime Supabase config copied into dist via Vite publicDir
+// This file is safe to edit for different environments.
+window.SUPABASE_CONFIG = {
+  url: "https://sigoorxtktxtbcneodux.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpZ29vcnh0a3R4dGJjbmVvZHV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0NzMwOTMsImV4cCI6MjA3MTA0OTA5M30.x1e1_9GNoNtQUF4EPYlSAf0HWfrwUzQAuwiWTnMhbN8"
+};
